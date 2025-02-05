8fa7853f21ee054242cb76660f34e8ae98e0a88e
Ajay-soni18 <ajaysoni7296@gmail.com>
2025-02-05 09:49:55 +0530

    Configure server port using environment variable

--- 01_express/index.js
+++ 01_express/index.js
@@ -1,7 +1,8 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running at port : ${port}... `);
