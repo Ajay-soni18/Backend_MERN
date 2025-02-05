1f36d475a3a8f8f91c8e4816c9043961dfec783d
Ajay-soni18 <ajaysoni7296@gmail.com>
2025-02-05 10:26:25 +0530

    Add logging functionality using Winston and Morgan middleware

--- 01_express/index.js
+++ 01_express/index.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import express from "express";
 
+import logger from "./logger.js";
+import morgan from "morgan";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,6 +25,22 @@ app.listen(port, () => {
 
 app.use(express.json());
 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 let foodData = [];
 let nextId = 1;
 
